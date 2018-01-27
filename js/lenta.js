9ed1d04bf0fc29e26732aa1dd420208d4ca1f563
borna <jmihalje2@ffzg.hr>
2018-01-27 12:00:41 +0100

    lenta fiks

--- js/lenta.js
+++ js/lenta.js
@@ -31,3 +31,9 @@
   window.addEventListener("scroll", callbackFunc);
 
 })();
+
+ $('#return-to-top').click(function() { // When arrow is clicked
+            $('body,html').animate({
+                scrollTop: 0 // Scroll to top of body
+            }, 500);
+        });
