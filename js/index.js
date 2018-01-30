7809a040dab5f17c89791aa42748339e8ffbf744
borna <jmihalje2@ffzg.hr>
2018-01-30 19:25:45 +0100

    Touch - test

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function()
+{
+    $("body").mouseup(function(e)
+    {
+        var subject = $(".w3-modal"); 
+
+        if(e.target.id != subject.attr('class') && !subject.has(e.target).length)
+        {
+            subject.fadeOut();
+        }
+    });
+});
