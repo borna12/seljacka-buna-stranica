f223185930e751ab8471220090813ad149dc2ea2
borna <jmihalje2@ffzg.hr>
2018-02-01 22:33:36 +0100

    Dorade

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
-$(document).ready(function()
-{
-    $("body").mouseup(function(e)
-    {
-        var subject = $(".w3-modal"); 
+$(document).ready(function() {
+    $("body").mouseup(function(e) {
+        var subject = $(".w3-modal");
 
-        if(e.target.id != subject.attr('class') && !subject.has(e.target).length)
-        {
+        if (e.target.id != subject.attr('class') && !subject.has(e.target).length) {
             subject.fadeOut();
+            $("div").animate({
+                scrollTop: 0
+            }, "fast");
+            $("#video")[0].load();
+
         }
     });
 });
