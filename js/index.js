da4642de650f84b1f7e03d00ef709675394170ae
borna <jomihx@gmail.com>
2018-02-05 20:32:40 +0100

    zezanacija

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
     $("body").mouseup(function(e) {
+        var subject2 = $("#html5box-html5-lightbox");
         var subject = $(".w3-modal");
+        if (e.target.id != subject2.attr('id') && !subject2.has(e.target).length) {
 
-        if (e.target.id != subject.attr('class') && !subject.has(e.target).length) {
-            subject.fadeOut();
-            $("div").animate({
-                scrollTop: 0
-            }, "fast");
-           
-
-        }
-    });
+            if (e.target.id != subject.attr('class') && !subject.has(e.target).length) {
+                subject.fadeOut();
+                $("div").animate({
+                    scrollTop: 0
+                }, "fast");
+            }
+        };
+    })
 });
