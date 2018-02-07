e39bd3aad4156e4104e0ce57b3b9526320e4a103
Unknown <jomihx@gmail.com>
2018-02-07 23:28:12 +0100

    još sitnica

--- vjesala/js/index.js
+++ vjesala/js/index.js
@@ -411,7 +411,7 @@
                 slicice.splice(rand_broj, 1);
                 this.msgTitle.html("Čestitamo!");
                 // this is messy
-                this.msgText.html("Pogodili ste odgovor"+tocno+ "u <span class='highlight'>" + rating.guesses + "</span> pokušaja.<br>Rezultat: <span class='highlight'>" + rating.rating + "%</span><br> Broj točno riješenih riječi: <span class='highlight'>" + tocno + "/" + 10 + "</span>");
+                this.msgText.html("Pogodili ste odgovor <span class='highlight'>" + this.wrd.word + "</span><br> u <span class='highlight'>" + rating.guesses + "</span> pokušaja.<br>Rezultat: <span class='highlight'>" + rating.rating + "%</span><br> Broj točno riješenih riječi: <span class='highlight'>" + tocno + "/" + 10 + "</span>");
                 this.showMsg();
                 this.playSound("winSound");
                 postotak += rating.rating;
